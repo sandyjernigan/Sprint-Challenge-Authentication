70c6b73b634eeb87ec19d29dec990f634f39bcb1
Sandy <sandy.jernigan@gmail.com>
2019-09-15 21:13:12 -0400

    check if the user is logged in middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,29 @@
   before granting access to the next middleware/route handler
 */
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('./users-model.js');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token =  req.headers.authorization;
+  
+  // see if there is a token
+  if (token) { 
+    //  rehash the header + payload + secret and see if it matches our verify signature
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      // check if it is valid, error if not
+      if (err) {
+        res.status(401).json({ message: 'You shall not pass!' });
+      } else { 
+        // token is valid
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'Those without tokens shall not pass!' });
+  }
+
 };

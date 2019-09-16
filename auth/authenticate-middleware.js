6679dc20bd979056616f685c50f062ff39584277
Sandy <sandy.jernigan@gmail.com>
2019-09-15 21:27:46 -0400

    require('dotenv').config();

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -5,18 +5,19 @@
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
-const Users = require('./users-model.js');
+const jwtSecret = process.env.JWT_SECRET || 'secret should be set in env';
 
 module.exports = (req, res, next) => {
-  const token =  req.headers.authorization;
+  const token = req.headers.authorization;
   
   // see if there is a token
   if (token) { 
     //  rehash the header + payload + secret and see if it matches our verify signature
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+    console.log(jwtSecret)
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
       // check if it is valid, error if not
       if (err) {
+        console.log(err)
         res.status(401).json({ message: 'You shall not pass!' });
       } else { 
         // token is valid

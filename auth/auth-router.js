1bddc9494b1def3319a9b8a478d05f9891703874
Sandy <sandy.jernigan@gmail.com>
2019-09-15 20:46:09 -0400

    implement login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,6 +25,25 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // jwt should be generated
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Request. Please check the username and password submitted.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 function generateToken(user) {

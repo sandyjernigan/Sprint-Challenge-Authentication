45a4fc6eb395fede512afdadca38d6b3f1588db6
Sandy <sandy.jernigan@gmail.com>
2019-09-15 20:21:25 -0400

    Update auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// User Models
+const Users = require('./users-model.js');
 
 router.post('/register', (req, res) => {
   // implement registration
@@ -7,5 +12,20 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
   // implement login
 });
+
+function generateToken(user) {
+  const payload = {
+    sub: 'user token',
+    id: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: '1d', // expires in 1 day
+  };
+
+  // extract the secret away so it can be required and used where needed
+  return jwt.sign(payload, process.env.JWT_SECRET, options); // this method is synchronous
+}
 
 module.exports = router;
